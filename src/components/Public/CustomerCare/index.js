6eebc5953a6f597e626d42552f53653791690cc9
dhanisthlm <dhanisthlm@gmail.com>
2017-12-06 15:18:58 +0100

    css

--- src/components/Public/CustomerCare/index.js
+++ src/components/Public/CustomerCare/index.js
@@ -25,11 +25,11 @@ export class CustomerCare extends Component {
                     <p className="text">Zdravlje.nu smo posvećeni pružanju najboljih mogućih psihološki savjetovanja za sve naše kliente. Da bismo to postigli, mi stalno tražimo da poboljšamo uslugu koju nudimo i stoga cjenimo sve povratne informacije, i dobre i loše.</p>
                     <h4 className="contact-header">Kontakt</h4>
                     <p className="text">Našu službu za korisnike možete kontaktirati putem emaila, Skypea ili pozivom na sljedeće telefonske brojeve:</p>
-                    <p className="text">E-pošta: <span className="email">zdravlje.nu@hotmail.com</span></p>
                     <h5 className="phone-header">Telefoni</h5>
-                    <p className="text phone-number">+387 603 21 22 90;</p>
+                    <p className="text phone-number">+387 603 21 22 90</p>
                     <p className="text phone-number">+387 66 23 60 83</p>
                     <p className="text">Besplatan razgovor preko Skypea: <span className="skype">zdravlje.nu@hotmail.com</span></p>
+                    <p className="text email">E-pošta: <span className="email">zdravlje.nu@hotmail.com</span></p>
                 </div>
             </div>
         )
