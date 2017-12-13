66a0cc6073c030a4458fdc251d70836e8cf2dd71
dhanisthlm <dhanisthlm@gmail.com>
2017-12-13 04:02:02 +0100

    footer

--- src/components/Public/Footer/index.js
+++ src/components/Public/Footer/index.js
@@ -21,11 +21,19 @@ export class Footer extends Component {
         return (
             <footer className="footer">
                 <div className="col col-1">
-                    <h3>O Zdravlje.nu</h3>
+                    <h2>O Zdravlje.nu</h2>
                     <p>Mi smo praksa za svakodnevne probleme koje većina nas susreće u našim životima. Naš cilj je da savjetodavne usluge budu pristupačne za sve, diskretno i po razumnoj cijeni. Korišćenjem naučnih i evidencijom dokazane interventcije, možemo pomoći ljudima da nauče da prevaziđu svoje probleme i nauče da žive život koji žele živjeti, udobno i samopouzdano.</p>
+                    <div className="address">
+                        <h3>Adresa</h3>
+                        <p className="address">Baunad doo Tuzla</p>
+                        <p className="address">Zdravlje.nu</p>
+                        <p className="address">Marsala Tita 109</p>
+                        <p className="address">75000 Tuzla</p>
+                        <p>Bosnia and Herzegovina</p>
+                    </div>
                 </div>
                 <div className="col col-2">
-                    <h3>Blogovi</h3>
+                    <h2>Blogovi</h2>
                     {
                         this.props.blogs.map((blog, i) => {
                             const href = `/blogovi#${blog.hash}`;
@@ -36,7 +44,7 @@ export class Footer extends Component {
                     }
                 </div>
                 <div className="col col-3">
-                    <h3>Uslovi</h3>
+                    <h2>Uslovi</h2>
                     <a className="link" href="/politika-privatnosti">Pravila o privatnosti</a>
                     <p className="link">Uslovi</p>
                     <p className="link">Izjava o kolačićima</p>
