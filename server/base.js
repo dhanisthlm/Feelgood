13e33178f341ae748ccb48f52cbd773ec705e8e0
dhanisthlm <dhanisthlm@gmail.com>
2017-12-30 23:52:34 +0100

    meta

--- server/base.js
+++ server/base.js
@@ -53,14 +53,16 @@ exports.register = function (server, options, next) {
             handler: (request, reply) => {
                 if ((process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'stage') &&
                     request.headers['x-forwarded-proto'] && request.headers['x-forwarded-proto'] === "http") {
-                    return reply().redirect(config.get('baseUrl'));
+                        if (request.path.includes('google43bf8a2e6701fef2')) {
+                            return reply.file('client/google43bf8a2e6701fef2.html');
+                        } else {
+                            return reply().redirect(config.get('baseUrl'));
+                        }
                 } else {
                     if (request.path.includes('pki-validation')) {
                         return reply.file('client/.well-known/pki-validation/godaddy.html');
                     } else if (request.path.includes('BingSiteAuth')) {
                         return reply.file('client/BingSiteAuth.xml');
-                    } else if (request.path.includes('google43bf8a2e6701fef2')) {
-                        return reply.file('client/google43bf8a2e6701fef2.html');
                     } else {
                         return reply.file('client/index.html');
                     }
