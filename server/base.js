618a4a48388534d93937ced60b47d2f907c58614
dhanisthlm <dhanisthlm@gmail.com>
2018-01-01 12:42:38 +0100

    server

--- server/base.js
+++ server/base.js
@@ -53,7 +53,11 @@ exports.register = function (server, options, next) {
             handler: (request, reply) => {
                 if ((process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'stage') &&
                     request.headers['x-forwarded-proto'] && request.headers['x-forwarded-proto'] === 'http') {
-                    return reply().redirect(config.get('baseUrl'));
+                        if (request.path.includes('google43bf8a2e6701fef2')) {
+                            return reply.file('client/google43bf8a2e6701fef2.html');
+                        } else {
+                            return reply().redirect(config.get('baseUrl'));
+                        }
                 } else {
                     if (request.path.includes('pki-validation')) {
                         return reply.file('client/.well-known/pki-validation/godaddy.html');
